b24411737c0681a601a36594276e8e8e8bdd86c1
Giovanny Santos <geovanny_555@outlook.com>
2026-03-10 16:37:24 -0300

    exercicios

--- exercicio030.py
+++ exercicio030.py
@@ -1,4 +1,4 @@
-print(" escreva a porcentagem de crescimento da produção: ")
+print("escreva a porcentagem de crescimento da produção: ")
 produção = float(input())
 if produção > 0:
     print("A produção está crescendo.")
@@ -6,4 +6,3 @@ elif produção < 0:
     print("A produção está diminuindo.")
 else:
     print("A produção está estável.")
-    

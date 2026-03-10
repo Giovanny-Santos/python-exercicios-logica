b5fdbf5496ca2714c5d57bb6de6f430e26d245d3
Giovanny Santos <geovanny_555@outlook.com>
2026-03-10 18:04:08 -0300

    exercicios

--- exercicio040.py
+++ exercicio040.py
@@ -1,16 +1,12 @@
-soma = 0       # vai somar todas as temperaturas válidas
-contador = 0   # vai contar quantas temperaturas foram digitadas
-
+# refazer esse exercicio depois de aprender a usar listas, para armazenar as temperaturas e calcular a média depois
+soma = 0
+contador = 0 
 while True:
     temperatura = float(input("Digite a temperatura em Celsius (-273 para sair): "))
-    
-    if temperatura == -273:  # condição de parada
+    if temperatura == -273:
         break
-    
-    soma += temperatura      # adiciona a temperatura à soma
-    contador += 1            # incrementa o contador
-
-# verificar se houve pelo menos uma temperatura válida
+    soma += temperatura
+    contador += 1
 if contador > 0:
     media = soma / contador
     print("Média das temperaturas:", media)
